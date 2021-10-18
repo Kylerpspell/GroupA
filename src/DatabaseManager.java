fd020b26878354613282036433ae3040b6b95021
Jack-Faust <rjfausty@gmail.com>
2021-10-17 21:08:21 -0400

    Created singleton DatabaseManager class and made method stubs

---
+++ src/DatabaseManager.java
@@ -0,0 +1,61 @@
+
+import java.util.ArrayList;
+
+public class DatabaseManager {
+    public static DatabaseManager databaseManager;
+    private ArrayList<Account> accounts;
+    private ArrayList<Job> jobs;
+    
+    public DatabaseManager() {
+        this.accounts = new ArrayList<Account>();
+        this.jobs = new ArrayList<Job>();
+    }
+    public static DatabaseManager getInstance() {
+        if (databaseManager == null) {
+            databaseManager = new DatabaseManager();
+        }
+        return databaseManager;
+    }
+
+    public ArrayList<Account> getAccounts(Account currentUser) {
+        return this.accounts;
+    }
+
+    public ArrayList<Job> getJobs(Account currentUser) {
+        return this.jobs;
+    }
+
+    public void addAccount(Account account, Account currentUser) {
+        this.accounts.add(account);
+    }
+
+    public void addJob(Job job, Account currentUser) {
+        this.jobs.add(job);
+    }
+
+    public void removeAccount(Account account, Account currentUser) {
+        this.accounts.remove(account);
+    }
+
+    public void removeJob(Job job, Account currentUser) {
+        this.jobs.remove(job);
+    }
+
+    public void updateAvailability(Account currentUser, Job jobToChange, boolean availabiltiy) {
+
+    }
+
+    public void updateVisibility(Account currentUser, Job jobToChange, boolean availabiltiy) {
+
+    }
+
+    public void changePassword(Account currentUser, Account account, String newPassword) {
+
+    }
+    public void addExternalDocument(Account currentUser, String documents) {
+
+    }
+    public void removeExternalDocument(Account currentUser, String documents) {
+
+    }
+}
