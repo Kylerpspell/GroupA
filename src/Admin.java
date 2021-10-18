84204e5d59a14cbe98970a684e13163a2a3f53b0
titan brown <tkennett@email.sc.edu>
2021-10-18 00:48:57 -0400

    Created basic Admin class stub

---
+++ src/Admin.java
@@ -0,0 +1,32 @@
+package src;
+public class Admin {
+
+    public void createAccount(Account currentUser){
+
+    }
+
+    public void editAccount(Account currentUser){
+
+    }
+
+    public void removeAccount(Account currentUser){
+
+    }
+
+    public void editResume(Resume resume){
+
+    }
+
+    public void removeResume(Resume resume){
+
+    }
+
+    public void editJob(Job job){
+        Employer editJob;
+    }
+
+    public void removeJobListing(Job job){
+        Employer removeJobListing;
+    }
+
+}
