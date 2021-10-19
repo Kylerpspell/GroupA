248fd2083adf726f42683fc54f569f01cc13a720
ocaldwell01 <odarriuscaldwell01@gmail.com>
2021-10-19 09:38:14 -0400

    Started to implement student and employer

--- src/Employer.java
+++ src/Employer.java
@@ -5,31 +5,33 @@ public class Employer {
     String companyWebsite;
     String companyDescription;
     public Employer(String companyName, String companyWebsite, String companyDescription){
-
+        /*
+        LINK METHODS TO CURRENT USER
+         */
     }
     public String getName(Account currentUser){
-        return "null";
+
     }
     public String getCompanyName(Account currentUser){
-        return "null";
+        return this.companyName;
     }
     public String getCompanyDescription(Account currentUser){
-        return "null";
+        return this.companyDescription;
     }
     public String getCompanyWebsite(Account currentUser){
-        return "null";
+        return this.companyWebsite;
     }
     public void setName(Account currentUser, String newName){
-
+        this.companyName = newName;
     }
     public void setCompanyName(Account currentUser, String newCompanyName){
-
+        this.companyName = newCompanyName;
     }
     public void setCompanyWebsite(Account currentUser, String newCompanyWebsite){
-
+        this.companyWebsite = newCompanyWebsite;
     }
     public void setCompanyDescription(Account currentUser, String newCompanyDescription){
-
+        this.companyDescription = newCompanyDescription;
     }
 
 }
