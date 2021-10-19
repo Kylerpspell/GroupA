a81397bac8d4066a603540d38e87fae3cc756fff
Jack-Faust <rjfausty@gmail.com>
2021-10-19 09:48:52 -0400

    Implementing create account and create job in interface manager

--- src/AccountType.java
+++ src/AccountType.java
@@ -3,5 +3,5 @@ public enum AccountType {
 	ACCOUNT_TYPE_STUDENT,
 	ACCOUNT_TYPE_EMPLOYER,
 	ACCOUNT_TYPE_ADMIN,
-	acoount_type_invalid;
+	account_type_invalid;
 }
--- src/InterfaceManager.java
+++ src/InterfaceManager.java
@@ -1,3 +1,8 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import src.Account;
+import src.AccountType;
 
 public class InterfaceManager {
     
@@ -20,11 +25,56 @@ public class InterfaceManager {
     public void logout() {
 
     }
-    public Account createAccount(String email, String password, String accountType) {
-        return new Account();
+    public Account createAccount() {
+        Scanner key = new Scanner(System.in);
+        AccountType accountType = null;
+        System.out.println("What type of account would you like to create?  \nEnter the appropriate number for your selection. \n1. Student \n2. Employer \n3. Application Administrator");
+        switch (key.nextInt()) {
+            case 1: 
+                accountType = ACCOUNT_TYPE_STUDENT;
+                break;
+            case 2:
+                accountType = ACCOUNT_TYPE_EMPLOYER;
+                break;
+            case 3:
+                accountType = ACCOUNT_TYPE_ADMIN;
+                break;
+        }
+        boolean validEmail = true;
+        String email = "";
+        do {
+        System.out.println("Please enter a valid email for the account");
+        email = key.nextLine();
+        int sub = email.length() - "sc.edu".length();
+        if (!email.substring(sub).equalsIgnoreCase("sc.edu")) {
+            System.out.println("Invalid email, please re-enter.");
+            validEmail = false;
+        }
+        } while (!validEmail);
+        System.out.println("Please enter a password for the account");
+        String password = key.nextLine();
+
+        return new Account(password, email, accountType);
     }
-    public Job createJob(Account currentUser, Account postingEmployer, boolean isAvailable, boolean isVisible) {
-        return new Job(currentUser, "", "");
+    public Job createJob(Account currentUser) {
+        Scanner key = new Scanner(System.in);
+        System.out.println("Please enter a job title for this listing.");
+        String jobTitle = key.nextLine();
+        System.out.println("Please enter a job description for this listing.");
+        String jobDescription = key.nextLine();
+        System.out.println("Would you like this listing to be available for application?. \nEnter the appropriate number for your selection. \n1. Yes \n2. No");
+        boolean isAvailable = false;
+        if (key.nextInt() == 1) {
+            key.nextLine();
+            isAvailable = true;
+        }
+        System.out.println("Would you like this listing to be available for viewing?. \nEnter the appropriate number for your selection. \n1. Yes \n2. No");
+        boolean isVisible= false;
+        if (key.nextInt() == 1) {
+            key.nextLine();
+            isVisible = true;
+        }
+        return new Job(jobTitle, jobDescription, currentUser, new ArrayList<Account>(), isAvailable, isVisible);
     }
     public void viewJobs(Account currentUser) {
 
