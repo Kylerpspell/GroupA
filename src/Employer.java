3a9dcace6f9ef3777faae9980e97a97eae7b85ce
ocaldwell01 <odarriuscaldwell01@gmail.com>
2021-10-27 00:12:27 -0400

    Commented Student, Employer and fixed some Student return methods

--- src/Employer.java
+++ src/Employer.java
@@ -17,11 +17,20 @@ public class Employer extends Account {
     public String getCompanyName(){
         return this.companyName;
     }
+    /*
+    returns name of company
+     */
     public String getCompanyDescription(){
         return this.companyDescription;
+        /*
+    returns description of company
+     */
     }
     public String getCompanyWebsite(){
         return this.companyWebsite;
+        /*
+    returns website of company
+     */
     }
     /*public void setName(Account currentUser, String newName){
         this.companyName = newName;
@@ -29,16 +38,28 @@ public class Employer extends Account {
     public void setCompanyName(String newCompanyName){
         this.companyName = newCompanyName;
     }
+    /*
+    takes in company name in parameter and sets it to current variable of company name
+     */
     public void setCompanyWebsite(String newCompanyWebsite){
         this.companyWebsite = newCompanyWebsite;
+         /*
+    takes in company website in parameter and sets it to current variable of company website
+     */
     }
     public void setCompanyDescription(String newCompanyDescription){
         this.companyDescription = newCompanyDescription;
+         /*
+    takes in company description in parameter and sets it to current variable of company description
+     */
     }
-    public void giveStudentRating(Employer employerAccount, Student studentAccount, double Rating){
+    public void giveStudentRating(Student studentAccount, double Rating){
         if( Rating >= 1 || Rating <= 5){
 			studentAccount.addRating(Rating);
         }
     }
+    /*
+        takes student account in parameter to locate the student and adds specified rating to student account
+     */
 }
 
