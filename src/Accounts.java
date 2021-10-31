d364df5a645f594090b81b85522fb10c4a43c4c3
Jack-Faust <rjfausty@gmail.com>
2021-10-31 17:47:01 -0400

    Javadoc comments added to Accounts and Admin classes

--- src/Accounts.java
+++ src/Accounts.java
@@ -6,10 +6,16 @@ public class Accounts {
 	private static Accounts accounts;
 	private ArrayList<Account> accountList;
 	
+	/**
+	 * Default account constructor
+	 */
 	private Accounts() {
 		accountList = DataLoader.getAccounts();
 	}
 	
+	/**
+	 * gets an instance of the Accounts class if accounts is null, makes accounts a singleton 
+	 */
 	public static Accounts getInstance() {
 		if(accounts == null) {
 			accounts = new Accounts();
@@ -18,6 +24,11 @@ public class Accounts {
 		return accounts;
 	}
 
+	/**
+	 * Checks to see if an account already exists for an email
+	 * @param email email to be checked if an account exists for it 
+	 * @return boolean that is true when the email does have an account and false otherwise
+	 */
 	public boolean haveAccount(String email) {
 		for(Account account : accountList) {
 			if(account.getEmail().equals(email)) {
@@ -28,6 +39,11 @@ public class Accounts {
 		return false;
 	}
 	
+	/**
+	 * gets an account based on the email linked to it
+	 * @param email email to retrieve account for
+	 * @return Account that utilizes that email
+	 */
 	public Account getAccount(String email) {
 		for(Account account : accountList) {
 			if(account.getEmail().equals(email)) {
@@ -38,10 +54,19 @@ public class Accounts {
 		return null;
 	}
 	
+	/**
+	 * Accessor method for all accounts
+	 * @return ArrayList of Accounts with all saved accounts
+	 */
 	public ArrayList<Account> getAccounts() {
 		return accountList;
 	}
 	
+	/**
+	 * adds an account to the accounts array list 
+	 * @param newAccount account to be added
+	 * @return returns true when the new account does not already exist and the new account is added, false otherwise
+	 */
 	public boolean addAccount(Account newAccount) {
 		if(haveAccount(newAccount.getEmail()))return false;
 		
@@ -49,6 +74,11 @@ public class Accounts {
 		return true;
 	}
 
+	/**
+	 * Removes an account using its UUID 
+	 * @param id UUID of the account
+	 * @return true when the account is found and removed, false otherwise
+	 */
 	public boolean removeAccount(UUID id) {
 		for(Account account : accountList) {
 			if(account.getId().equals(id)) {
@@ -59,6 +89,9 @@ public class Accounts {
 		return false;
 	}
 	
+	/**
+	 * Utilizes the data writer to save accounts to a JSON file
+	 */
 	public void saveAccounts() {
 		DataWriter.saveAccounts();
 	}
--- src/Admin.java
+++ src/Admin.java
@@ -7,17 +7,31 @@ public class Admin implements Account{
 	private String password;
 	private UUID id;
 
-	Admin(UUID id, String email, String password) {
+	/**
+	 * Parameterized constructor for Admin 
+	 * @param id UUID  id of the admin
+	 * @param email  String email of the admin
+	 * @param password String password of the admin
+	 */
+	public Admin(UUID id, String email, String password) {
 		this.email = email;
 		this.password = password;
 		this.id = id;
 	}
 
 
+	/**
+	 * Accessor method for password
+	 * @return String Admin password 
+	 */
 	public String getPassword(){
 		return password;
 	}
 	
+	/**
+	 * Mutator method for password
+	 * @param password  String password to set the admin's password to
+	 */
 	public void setPassword(String password){
 		if(DatabaseManager.validPassword(password)) {
 			this.password = password;
@@ -25,10 +39,18 @@ public class Admin implements Account{
 		System.out.println("Invalid password");
 	}
 
+	/**
+	 * Accessor method for email
+	 * @return String admin email
+	 */
 	public String getEmail(){
 		return email;
 	}
-
+	
+	/**
+	 * Mutator method for email
+	 * @param email  String email to set the admin's email to
+	 */
 	public void setEmail(String email){
 		if(DatabaseManager.validEmail(email)) {
 			this.email = email;
@@ -36,14 +58,26 @@ public class Admin implements Account{
 		System.out.println("Invalid email");
 	}
 
+	/**
+	 * Accessor method for account type 
+	 * @return always returns Admin account type
+	 */
 	public AccountType getAccountType(){
 		return AccountType.ACCOUNT_TYPE_ADMIN;
 	}
 
+	/**
+	 * Accessor method for id 
+	 * @return UUID id linked to admin
+	 */
 	public UUID getId(){
 		return id;
 	}
 
+	/**
+	 * toString account that prints admins email
+	 * @return String containing admin information
+	 */
 	@Override
 	public String toString(){
 		return "Admin: " + email;
