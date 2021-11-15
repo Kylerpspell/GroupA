8d5f782e1b479b9d5880cd0f2961b14773ad774f
Jack-Faust <rjfausty@gmail.com>
2021-11-14 21:40:53 -0500

    Added more to testing

--- src/DatabaseManagerTest.java
+++ src/DatabaseManagerTest.java
@@ -1,5 +1,9 @@
 package src;
 import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.UUID;
+
 import org.junit.Test;
 import org.junit.jupiter.api.AfterEach;
 
@@ -18,4 +22,37 @@ public class DatabaseManagerTest {
         assertTrue(dm.validPassword("thisIsaValidPassword"));
         assertFalse(dm.validPassword("nonval"));
     }
+
+    @Test 
+    public void TestAddAccount() {
+        int size = dm.getAccounts().getAccounts().size();
+        dm.addAccount(new Student());
+        int size2 = dm.getAccounts().getAccounts().size();
+        assertNotEquals(size, size2);
+    }
+
+    @Test 
+    public void TestAddJobs() {
+        int size = dm.getJobs().getJobs().size();
+        dm.addJob(new Job(UUID.randomUUID(), "New job", "new Job", new Employer(), new ArrayList<Student>(), true, true));
+        int size2 = dm.getJobs().getJobs().size();
+        assertNotEquals(size, size2);
+    }
+
+    @Test
+    public void TestRemoveAccount() {
+        int size = dm.getAccounts().getAccounts().size();
+        dm.removeAccount(new Student());
+        int size2 = dm.getAccounts().getAccounts().size();
+        assertNotEquals(size, size2);
+    }
+
+    @Test 
+    public void TestRemoveJobs() {
+        int size = dm.getJobs().getJobs().size();
+        dm.removeJob(new Job(UUID.randomUUID(), "New job", "new Job", new Employer(), new ArrayList<Student>(), true, true));
+        int size2 = dm.getJobs().getJobs().size();
+        assertNotEquals(size, size2);
+    }
+
 }
