ccc113b08010b1f3d935b56d8583a01c6d9bb1ae
Jack-Faust <rjfausty@gmail.com>
2021-11-15 14:40:21 -0500

    additional testing of database Manager

--- src/DatabaseManagerTest.java
+++ src/DatabaseManagerTest.java
@@ -10,7 +10,8 @@ import org.junit.jupiter.api.AfterEach;
 public class DatabaseManagerTest {
     
     DatabaseManager dm = DatabaseManager.getInstance();
-
+    Student student = new Student();
+    Job job = new Job(UUID.randomUUID(), "New job", "new Job", new Employer(), new ArrayList<Student>(), true, true);
     @Test
     public void TestValidEmail() {
         assertTrue(dm.validEmail("email@email.sc.edu"));
@@ -24,35 +25,59 @@ public class DatabaseManagerTest {
     }
 
     @Test 
-    public void TestAddAccount() {
+    public void testAddAccount() {
         int size = dm.getAccounts().getAccounts().size();
-        dm.addAccount(new Student());
+        dm.addAccount(student);
         int size2 = dm.getAccounts().getAccounts().size();
         assertNotEquals(size, size2);
     }
 
     @Test 
-    public void TestAddJobs() {
+    public void testAddJobs() {
         int size = dm.getJobs().getJobs().size();
-        dm.addJob(new Job(UUID.randomUUID(), "New job", "new Job", new Employer(), new ArrayList<Student>(), true, true));
+        dm.addJob(job);
         int size2 = dm.getJobs().getJobs().size();
         assertNotEquals(size, size2);
     }
 
     @Test
-    public void TestRemoveAccount() {
+    public void testRemoveAccount() {
         int size = dm.getAccounts().getAccounts().size();
-        dm.removeAccount(new Student());
+        dm.removeAccount(student);
         int size2 = dm.getAccounts().getAccounts().size();
         assertNotEquals(size, size2);
     }
 
     @Test 
-    public void TestRemoveJobs() {
+    public void testRemoveJobs() {
         int size = dm.getJobs().getJobs().size();
-        dm.removeJob(new Job(UUID.randomUUID(), "New job", "new Job", new Employer(), new ArrayList<Student>(), true, true));
+        dm.removeJob(job);
         int size2 = dm.getJobs().getJobs().size();
         assertNotEquals(size, size2);
+    }
+
+    @Test
+    public void testChangePassword() {
+        dm.changePassword(student, "ValidPassword");
+        assertNotNull(student.getPassword());
+        dm.changePassword(student, null);
+        assertNull(student.getPassword());
+    }
+
+    @Test
+    public void testUpdateAvailability() {
+        dm.updateAvailability(job, false);
+        assertFalse(job.checkAvailability());
+        dm.updateAvailability(job, true);
+        assertTrue(job.checkAvailability());
+    }
+
+    @Test
+    public void testUpdateVisibility() {
+        dm.updateVisibility(job, false);
+        assertFalse(job.checkVisibility());
+        dm.updateVisibility(job, true);
+        assertTrue(job.checkVisibility());
     }
 
 }
